2d864763b1782fdd087911059085dd3876c5c9f7
JJ <jj.spetseris@yahoo.com>
2017-07-05 14:04:45 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import './App.css';
+import getMuiTheme from 'material-ui/styles/getMuiTheme';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import AppBar from 'material-ui/AppBar';
+import {indigo500, indigo100, indigo900, grey500, grey700, white} from 'material-ui/styles/colors';
+// import Contacts from './todo.jsx'
+
+//import {auth} from './firebase';
+
+import injectTapEventPlugin from 'react-tap-event-plugin';
+// Needed for onTouchTap
+// http://stackoverflow.com/a/34015469/988941
+injectTapEventPlugin();
+
+const muiTheme = getMuiTheme({
+  palette: {
+    primary1Color: indigo500,
+    primary2Color: indigo100,
+    primary3Color: indigo900,
+    accent1Color: grey500,
+    accent2Color: grey700,
+    alternateTextColor: white,
+    labelColor: white,
+  }
+})
+
+class App extends Component {
+  render() {
+    return (
+      <MuiThemeProvider muiTheme={muiTheme}>
+        <main>
+          <AppBar title="TODO APP" />
+            <div>
+              It Works!
+            </div>
+        </main>
+      </MuiThemeProvider>
+    );
+  }
+}
+
+export default App;
---
+++ src/todo.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import TextField from 'material-ui/TextField';
+import RaisedButton from 'material-ui/RaisedButton';
+import './todo.css';
+
+
+class Todo extends Component {
+  render() {
+    return (
+      
+    )
+  };
+}
+
+export default Todo;
