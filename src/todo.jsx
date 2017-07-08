ad16544be461a200267379071a93f35fb6e6e97c
JJ <jj.spetseris@yahoo.com>
2017-07-08 12:36:37 -0500

    added basic drag functinoality need drop

--- src/todo.jsx
+++ src/todo.jsx
@@ -7,6 +7,8 @@ const style = {
   marginBottom: 20,
 };
 
+// add drop functions (basic script won't work)
+
 class Todo extends Component {
   render() {
     return (
@@ -14,7 +16,8 @@ class Todo extends Component {
         <Card className='card'>
           <CardTitle title="Uncompleted Tasks"/>
           <ol id='uTask' ondragover="allowDrop(event)" ondrop="drop(event)">
-            <li>need to add</li>
+            <li draggable="true" ondragstart="drag(event)">Example 1</li>
+            <li draggable="true" ondragstart="drag(event)">Example 2</li>
           </ol>
           <RaisedButton className='add' id='add' label='+Add' primary={true} style={style}/>
         </Card>
@@ -22,7 +25,8 @@ class Todo extends Component {
         <Card className='card'>
           <CardTitle title="Completed Tasks"/>
           <ol id='cTask' ondragover="allowDrop(event)" ondrop="drop(event)">
-            <li>need to add</li>
+            <li draggable="true" ondragstart="drag(event)">Example 3</li>
+            <li draggable="true" ondragstart="drag(event)">Example 4</li>
           </ol>
           <RaisedButton className='add' id='add' label='+Add' primary={true} style={style}/>
         </Card>
